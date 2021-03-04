78ff1fd43f56d822a30985504ed178f76e53d76f
Joshua Zuke <zuke.joshua@gmail.com>
2021-03-04 19:46:03 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function takeANumber(katzDeliLineline, name){
   for (let i = 1; i < katzDeliLineline.length; i++){
     katzDeliLineline.push(name)
-
+  }
     return `Welcome, ${name}. You are number ${i} in line.`
 }
