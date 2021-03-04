22f156554985ec7903dafbfd41aec9cf89bd9346
Joshua Zuke <zuke.joshua@gmail.com>
2021-03-04 19:57:16 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,5 +4,15 @@ function takeANumber(katzDeliLineline, name){
 }
 
 function nowServing(katzDeliLineline){
-  
+  if (katzDeliLineline.length > 0){
+    // some code to do what it says below
+  }
+} else {
+  return "There is nobody waiting to be served!"
 }
+
+// Build a function nowServing. This function should:
+// accept the current line of people (katzDeliLine) and return the first person in line and then remove that individual from the line.
+// If there is nobody in line, it should return "There is nobody waiting to be served!"
+
+// maybe an if/else, with array.length greater than 0 as the condition
